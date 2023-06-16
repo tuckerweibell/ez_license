32dcda868f9efb9c5e544d6da5cba10e3138cca7
tuckerweibell <tucker.weibell@ezcater.com>
2023-06-16 14:48:19 -0600

    updates

--- pip_handler.sh
+++ pip_handler.sh
@@ -4,7 +4,7 @@ handle_pip_deps () {
 
     if pip_installed; then
         pip install -U pip-licenses
-        pip-licenses -l --format=json > ez_license/dependencies/python_deps.json
+        pip-licenses -u --format=json > ez_license/dependencies/python_deps.json
     else
         printf '\n'; echo "Skipping pip - not installed."
     fi
