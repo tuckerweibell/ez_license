631314ff78cd7f0e447bf9a61e4be7ca0832237d
tuckerweibell <tucker.weibell@ezcater.com>
2023-06-16 16:12:02 -0600

    updates

--- check_pkg_mg.sh
+++ check_pkg_mg.sh
@@ -3,15 +3,15 @@
 
 check_pkg_managers() {
     
-    yarn -v &>/dev/null; if [ $? == 0 ] && [ "$YARN" != "y" ]; then YARN="y" && echo "Yarn detected."; else YARN="n"; fi
+    yarn -v &>/dev/null; if [ $? == 0 ] && [ "$YARN" != "n" ]; then YARN="y" && echo "Yarn detected."; else YARN="n"; fi
 
-    npm -v &>/dev/null; if [ $? == 0 ] && [ "$NPM" != "y" ]; then NPM="y" && echo "Npm detected."; else NPM="n"; fi
+    npm -v &>/dev/null; if [ $? == 0 ] && [ "$NPM" != "n" ]; then NPM="y" && echo "Npm detected."; else NPM="n"; fi
 
-    gem -v &>/dev/null; if [ $? == 0 ] && [ "$GEM" != "y" ]; then GEM="y" && echo "Gems detected."; else GEM="n"; fi
+    gem -v &>/dev/null; if [ $? == 0 ] && [ "$GEM" != "n" ]; then GEM="y" && echo "Gems detected."; else GEM="n"; fi
 
-    pip -v &>/dev/null; if [ $? == 0 ] && [ "$PIP" != "y" ]; then PIP="y" && echo "Pip detected."; else PIP="n"; fi
+    pip -v &>/dev/null; if [ $? == 0 ] && [ "$PIP" != "n" ]; then PIP="y" && echo "Pip detected."; else PIP="n"; fi
 
-    poetry -v &>/dev/null; if [ $? == 0 ] && [ "$POETRY" != "y" ]; then POETRY="y" && echo "Poetry detected."; else POETRY="n"; fi
+    poetry -v &>/dev/null; if [ $? == 0 ] && [ "$POETRY" != "n" ]; then POETRY="y" && echo "Poetry detected."; else POETRY="n"; fi
 
     printf '\n'
 }
