8591b4a0aebc634df4a1074899c56c4aeccc180d
tuckerweibell <tucker.weibell@ezcater.com>
2023-06-16 15:34:52 -0600

    updates

--- check_pkg_mg.sh
+++ check_pkg_mg.sh
@@ -3,12 +3,12 @@
 
 check_pkg_managers() {
     
-    if `yarn -v &>/dev/null`; then YARN="y" && echo "Yarn detected."; else YARN="n"; fi
+    if [ `yarn -v &>/dev/null` ]; then YARN="y" && echo "Yarn detected."; else YARN="n"; fi
 
-    if `npm -v &>/dev/null`; then NPM="y" && echo "Npm detected."; else NPM="n"; fi
+    if [ `npm -v &>/dev/null` ]; then NPM="y" && echo "Npm detected."; else NPM="n"; fi
 
-    if `gem -v &>/dev/null`; then GEM="y" && echo "Gems detected."; else GEM="n"; fi
+    if [ `gem -v &>/dev/null` ]; then GEM="y" && echo "Gems detected."; else GEM="n"; fi
 
-    if [ `pip -v &>/dev/null` || `poetry -v &>/dev/null`]; then PIP="y" && echo "Pip/Poetry detected."; else PIP="n"; fi
+    if [ `pip -v &>/dev/null` || `poetry -v &>/dev/null` ]; then PIP="y" && echo "Pip/Poetry detected."; else PIP="n"; fi
 
 }
