ce63398e2c8919ad25a48d5f96270b335aaf2efd
tuckerweibell <tucker.weibell@ezcater.com>
2023-06-15 18:47:51 -0600

    updates

--- os_handler.sh
+++ os_handler.sh
@@ -2,20 +2,17 @@
 
 handle_os_input () {
 
-    echo 'Please select OS:'
-    echo '----------------'
-    printf '\n' 
-    echo '1) alpine' 
-    echo '2) debian' 
-    printf '\n'
-    read -p 'Enter: ' os_var
+    printf '\n'; echo 'Please select OS:'; echo '----------------'; printf '\n' 
+    echo '1) alpine' ; echo '2) debian'; printf '\n'; read -p 'Enter: ' os_var
+
     if [ $os_var == 1 ]; then
         if handle_alpine; then success_msg; else handle_fail; fi
     elif [ $os_var == 2 ]; then
         if handle_debian; then success_msg; else handle_fail; fi
     else
-        echo "Please select 1 or 2"; exit
+        echo "ERROR: Please select 1 or 2."; printf '\n'; exit
     fi
+
     printf '\n'
 
 }
@@ -25,6 +22,7 @@ handle_alpine () {
     apk update && apk add jq npm yarn
 
 }
+
 handle_debian () {
 
     apt-get update -y && apt install jq npm yarn -y
@@ -32,10 +30,13 @@ handle_debian () {
 }
 
 success_msg () {
+
     echo "Packages installed successfully."
+
 }
 
 handle_fail () {
-    echo "Packages failed to install. Please manually install npm, jq, and yarn."
-    exit
+
+    echo "Packages failed to install. Please manually install npm, jq, and yarn."; exit
+
 }
