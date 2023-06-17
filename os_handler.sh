98f853d00f441f9e0b70be45a653ffd4a63223cb
tuckerweibell <tucker.weibell@ezcater.com>
2023-06-17 10:25:01 -0600

    updates

--- os_handler.sh
+++ os_handler.sh
@@ -73,8 +73,18 @@ set_install () {
 }
 
 check_lock () {
-    ls 'package-lock.json' &>/dev/null; if [ $? -eq 0 ]; then NPM_INSTALL="y" && echo "npm will be installed."; else NPM_INSTALL="n"; fi
-    ls 'yarn.lock' &>/dev/null; if [ $? -eq 0 ]; then YARN_INSTALL="y" && echo "yarn will be installed."; else YARN_INSTALL="n"; fi
+    if [ $NPM == "n" ] then;
+        ls 'package-lock.json' &>/dev/null; 
+        if [ $? -eq 0 ]; then NPM_INSTALL="y" && echo "npm will be installed."; return 0; else NPM_INSTALL="n"; fi
+        ls 'package.json' &>/dev/null;
+        if [ $? -eq 0 ]; then NPM_INSTALL="y" && echo "npm will be installed."; return 0; else NPM_INSTALL="n"; fi
+        ls 'node_modules' &>/dev/null;
+        if [ $? -eq 0 ]; then NPM_INSTALL="y" && echo "npm will be installed."; return 0; else NPM_INSTALL="n"; fi
+    fi
+
+    if [ $YARN == "n" ] then;
+        ls 'yarn.lock' &>/dev/null; 
+        if [ $? -eq 0 ]; then YARN_INSTALL="y" && echo "yarn will be installed."; return 0; else YARN_INSTALL="n"; fi
 }
 
 clean_up () {
